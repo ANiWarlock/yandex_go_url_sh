3d5ea2c1298dd1b4a766dcbce95a366df6697a3f
zrkarimov <zrkarimov@mts.ru>
2023-05-13 23:04:11 +0300

    add tests

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,10 +1,6 @@
 package main
 
 import (
-	"crypto/sha1"
-	"encoding/hex"
-	"fmt"
-	"io"
 	"net/http"
 )
 
@@ -16,51 +12,10 @@ func main() {
 	linkStore = make(map[string]string)
 
 	mux := http.NewServeMux()
-	mux.HandleFunc(`/`, mainPage)
+	mux.HandleFunc(`/`, mainPageHandler)
 
 	err := http.ListenAndServe(`:8080`, mux)
 	if err != nil {
 		panic(err)
 	}
-}
-func mainPage(rw http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodPost {
-		defer r.Body.Close()
-		responseData, err := io.ReadAll(r.Body)
-		if err != nil {
-			http.Error(rw, fmt.Sprintf("Error in request body: %s", err), http.StatusBadRequest)
-			return
-		}
-
-		longURL := string(responseData)
-		hashedURL := shorten(longURL)
-		linkStore[hashedURL] = longURL
-		shortURL := baseURL + "/" + hashedURL
-
-		rw.WriteHeader(http.StatusCreated)
-		_, err = rw.Write([]byte(shortURL))
-		if err != nil {
-			return
-		}
-	} else if r.Method == http.MethodGet {
-		shortURL := r.URL.Path[1:]
-
-		if shortURL == "" {
-			rw.WriteHeader(http.StatusBadRequest)
-			return
-		}
-
-		rw.Header().Set("Location", linkStore[shortURL])
-		rw.WriteHeader(http.StatusTemporaryRedirect)
-		return
-	} else {
-		rw.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-}
-
-func shorten(longURL string) string {
-	hashedString := sha1.Sum([]byte(longURL))
-	return hex.EncodeToString(hashedString[:4])
 }
---
+++ cmd/shortener/main_page_handler_test.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+func Test_mainPageHandler(t *testing.T) {
+	url := "http://ya.ru"
+	type want struct {
+		code        int
+		contentType string
+		location    string
+	}
+	tests := []struct {
+		name   string
+		method string
+		url    string
+		body   string
+		want   want
+	}{
+		{
+			name:   "POST test #1",
+			method: http.MethodPost,
+			url:    "/",
+			body:   url,
+			want: want{
+				code:        201,
+				contentType: "text/plain; charset=utf-8",
+				location:    "",
+			},
+		},
+		{
+			name:   "POST test #2",
+			method: http.MethodPost,
+			url:    "/",
+			body:   "",
+			want: want{
+				code:        400,
+				contentType: "text/plain; charset=utf-8",
+				location:    "",
+			},
+		},
+		{
+			name:   "GET test #1",
+			method: http.MethodGet,
+			url:    "/shortened",
+			body:   "",
+			want: want{
+				code:        307,
+				contentType: "",
+				location:    url,
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			linkStore = make(map[string]string)
+			if test.method == http.MethodGet {
+				linkStore["shortened"] = url
+			}
+			request := httptest.NewRequest(test.method, test.url, strings.NewReader(test.body))
+			rr := httptest.NewRecorder()
+			mainPageHandler(rr, request)
+			res := rr.Result()
+			assert.Equal(t, test.want.code, res.StatusCode)
+
+			defer res.Body.Close()
+			resBody, err := io.ReadAll(res.Body)
+			require.NoError(t, err)
+
+			assert.Equal(t, test.want.contentType, rr.Header().Get("Content-Type"))
+
+			if test.method == http.MethodPost {
+				for key, _ := range linkStore {
+					assert.Equal(t, baseURL+"/"+key, string(resBody))
+				}
+			} else if test.method == http.MethodGet {
+				assert.Equal(t, test.want.location, rr.Header().Get("Location"))
+			}
+		})
+	}
+}
