d659a31857df15dd846b102f98b02aac8ad5ea19
zrkarimov <zrkarimov@mts.ru>
2023-05-30 16:52:11 +0300

    it8 add gzip compression

---
+++ router/middleware/logger.go
@@ -0,0 +1,55 @@
+package middleware
+
+import (
+	"github.com/ANiWarlock/yandex_go_url_sh.git/logger"
+	"net/http"
+	"time"
+)
+
+type (
+	responseData struct {
+		status int
+		size   int
+	}
+
+	loggingResponseWriter struct {
+		http.ResponseWriter
+		responseData *responseData
+	}
+)
+
+func (r *loggingResponseWriter) Write(b []byte) (int, error) {
+	size, err := r.ResponseWriter.Write(b)
+	r.responseData.size += size
+	return size, err
+}
+
+func (r *loggingResponseWriter) WriteHeader(statusCode int) {
+	r.ResponseWriter.WriteHeader(statusCode)
+	r.responseData.status = statusCode
+}
+
+func SugarLogger(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		responseData := &responseData{
+			status: 0,
+			size:   0,
+		}
+		lw := loggingResponseWriter{
+			ResponseWriter: w,
+			responseData:   responseData,
+		}
+		next.ServeHTTP(&lw, r)
+
+		duration := time.Since(start)
+
+		logger.Sugar.Infoln(
+			"uri", r.RequestURI,
+			"method", r.Method,
+			"status", responseData.status,
+			"duration", duration,
+			"size", responseData.size,
+		)
+	})
+}
--- router/router.go
+++ router/router.go
@@ -2,13 +2,14 @@ package router
 
 import (
 	"github.com/ANiWarlock/yandex_go_url_sh.git/internal/app"
-	"github.com/ANiWarlock/yandex_go_url_sh.git/logger"
+	"github.com/ANiWarlock/yandex_go_url_sh.git/router/middleware"
 	"github.com/go-chi/chi/v5"
 )
 
 func NewShortenerRouter(myApp *app.App) chi.Router {
 	r := chi.NewRouter()
-	r.Use(logger.MiddlewareLogger)
+	r.Use(middleware.SugarLogger)
+	r.Use(middleware.Gzip)
 
 	r.Post("/", myApp.GetShortURLHandler)
 	r.Post("/api/shorten", myApp.APIGetShortURLHandler)
